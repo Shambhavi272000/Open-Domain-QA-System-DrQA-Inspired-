337ec631e1deebfa151bbca7599f9d813fc5b5d6
Shambhavi <67188688+Shambhavi272000@users.noreply.github.com>
2021-08-09 02:03:31 +0530

    Update model.py

--- drqa/reader/model.py
+++ drqa/reader/model.py
@@ -1,4 +1,4 @@
-
+"""This file presents a DrQA document reader model."""
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
@@ -13,9 +13,16 @@ logger = logging.getLogger(__name__)
 
 
 class DocReader(object):
+    
+    """This is a high level model which handles the intialization process of the underlying network
+    architecture, saving, updating examples, and predicting examples.
+    """
 
+#Initialization is carried on below
     def __init__(self, args, word_dict, feature_dict,
                  state_dict=None, normalize=True):
+        
+        # Book-keeping.
         self.args = args
         self.word_dict = word_dict
         self.args.vocab_size = len(word_dict)
@@ -24,6 +31,9 @@ class DocReader(object):
         self.updates = 0
         self.use_cuda = False
         self.parallel = False
+        
+        # Here we are building network. If normalize if false, scores are not normalized
+        # 0-1 per paragraph (no softmax).
         if args.model_type == 'rnn':
             self.network = RnnDocReader(args, normalize)
         else:
@@ -37,8 +47,20 @@ class DocReader(object):
                 self.network.load_state_dict(state_dict)
 
     def dict_expand(self, words):
+        
+        """Here we are adding words to the DocReader dictionary if they do not exist. The
+        underlying embedding matrix is also expanded (with random embeddings).
+        Args:
+            words: iterable of tokens to add to the dictionary.
+        Output:
+            added: set of tokens that were added.
+        """
         to_add = {self.word_dict.normalize(w) for w in words
                   if w not in self.word_dict}
+        
+        # Adding words to dictionary and expanding the embedding layer
+
+        
         if len(to_add) > 0:
             logger.info('Adding %d new words to dictionary...' % len(to_add))
             for w in to_add:
@@ -55,11 +77,25 @@ class DocReader(object):
         return to_add
 
     def vec_embeddings_load(self, words, embedding_file):
+        
+        
+         """Loading pretrained embeddings for a given list of words, if they exist.
+        Arguments:
+            words: iterable of tokens. Only those that are indexed in the
+              dictionary are kept.
+            embedding_file: path to text file of embeddings, space separated.
+        """
+        
+        
         words = {w for w in words if w in self.word_dict}
         logger.info('Loading pre-trained embeddings for %d words from %s' %
                     (len(words), embedding_file))
         embedding = self.network.embedding.weight.data
 
+        # When normalized, some words are duplicated. (Average the embeddings).
+
+        
+        
         vec_counts = {}
         with open(embedding_file) as f:
             line = f.readline().rstrip().split(' ')
@@ -88,6 +124,20 @@ class DocReader(object):
                     (len(vec_counts), 100 * len(vec_counts) / len(words)))
 
     def embed_tune(self, words):
+        
+        
+        """Unfixing the embeddings of a list of words. This is only applicable if
+        only few embeddings are being tuned (tune_partial = N).
+        
+        Shuffles the N specified words to the front of the dictionary, and saves
+        the original vectors of the other N + 1:vocab words in a fixed buffer.
+        Arguments:
+            words: iterable of tokens contained in dictionary.
+        """
+        
+        
+        
+        
         words = {w for w in words if w in self.word_dict}
 
         if len(words) == 0:
@@ -97,11 +147,19 @@ class DocReader(object):
         if len(words) == len(self.word_dict):
             logger.warning('Tuning ALL embeddings in dictionary')
             return
+        # Mixing vectors with words
+
+        
         embedding = self.network.embedding.weight.data
         for idx, swap_word in enumerate(words, self.word_dict.START):
+            
+         # Getting current word + embedding for this index
             curr_word = self.word_dict[idx]
             curr_emb = embedding[idx].clone()
             old_idx = self.word_dict[swap_word]
+            
+          # Swapping embeddings + dictionary indices
+
             embedding[idx].copy_(embedding[old_idx])
             embedding[old_idx].copy_(curr_emb)
             self.word_dict[swap_word] = idx
@@ -113,6 +171,10 @@ class DocReader(object):
         )
 
     def init_optimizer(self, state_dict=None):
+        """Initializing an optimizer for the network's free parameters.
+        Args:
+            state_dict: network parameters
+        """
         if self.args.fix_embeddings:
             for p in self.network.embedding.parameters():
                 p.requires_grad = False
@@ -127,15 +189,19 @@ class DocReader(object):
         else:
             raise RuntimeError('Unsupported optimizer: %s' %
                                self.args.optimizer)
+            
+         #Learning is done here
+        
     def update(self, ex):
         """Forward a batch of examples; step the optimizer to update weights."""
         if not self.optimizer:
             raise RuntimeError('No optimizer set.')
 
-       
+       #Training mode is done here
         self.network.train()
 
-       
+        # Giving Transfer to GPU
+
         if self.use_cuda:
             inputs = [e if e is None else e.cuda(non_blocking=True)
                       for e in ex[:5]]
@@ -145,12 +211,15 @@ class DocReader(object):
             inputs = [e if e is None else e for e in ex[:5]]
             target_s = ex[5]
             target_e = ex[6]
-
-       
+            
+       # Running forward
+    
         score_s, score_e = self.network(*inputs)
 
-        
+        # Computing losses and accuracies
+
         loss = F.nll_loss(score_s, target_s) + F.nll_loss(score_e, target_e)
+        #  Removing gradients and run backward
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -159,7 +228,8 @@ class DocReader(object):
         torch.nn.utils.clip_grad_norm_(self.network.parameters(),
                                        self.args.grad_clipping)
 
-        
+         # Updating parameters
+
         self.optimizer.step()
         self.updates += 1
 
@@ -170,8 +240,10 @@ class DocReader(object):
 
     def para_reset(self):
         
+        """Resetting any partially fixed parameters to original states."""
 
-        
+         # Resetting fixed embeddings to original value
+
         if self.args.tune_partial > 0:
             if self.parallel:
                 embedding = self.network.module.embedding.weight.data
@@ -180,14 +252,29 @@ class DocReader(object):
                 embedding = self.network.embedding.weight.data
                 fixed_embedding = self.network.fixed_embedding
 
-            
+         # Embeddings to fix are the last indices
+
             offset = embedding.size(0) - fixed_embedding.size(0)
             if offset >= 0:
                 embedding[offset:] = fixed_embedding
 
     
     def predict(self, ex, candidates=None, top_n=1, async_pool=None):
-    
+        
+    """Forwarding a batch of examples only to get predictions.
+        Arguments:
+            ex: the batch
+            candidates: batch * variable length list of string answer options.
+              The model will only consider exact spans contained in this list.
+            top_n: Number of predictions to return per batch element.
+            async_pool: If provided, non-gpu post-processing will be offloaded
+              to this CPU process pool.
+        Output:
+            pred_s: batch * top_n predicted start indices
+            pred_e: batch * top_n predicted end indices
+            pred_score: batch * top_n prediction scores
+        If async_pool is given, these will be AsyncResult handles.
+        """
        
         self.network.eval()
 
@@ -200,8 +287,7 @@ class DocReader(object):
       
         with torch.no_grad():
             score_s, score_e = self.network(*inputs)
-
-        
+        # Decode predictions
         score_s = score_s.data.cpu()
         score_e = score_e.data.cpu()
         if candidates:
@@ -219,12 +305,21 @@ class DocReader(object):
 
     @staticmethod
     def decode(score_s, score_e, top_n=1, max_len=None):
+         """Taking argmax of constrained score_s * score_e.
+        Args:
+            score_s: independent start predictions
+            score_e: independent end predictions
+            top_n: number of top scored pairs to take
+            max_len: max span length to consider
+        """
         
         pred_s = []
         pred_e = []
         pred_score = []
         max_len = max_len or score_s.size(1)
         for i in range(score_s.size(0)):
+        # Outer product of scores to get full p_s * p_e matrix
+
             scores = torch.ger(score_s[i], score_e[i])
 
             scores.triu_().tril_(max_len - 1)
@@ -259,7 +354,8 @@ class DocReader(object):
                 cands = PROCESS_CANDS
             if not cands:
                 raise RuntimeError('No candidates given.')
-
+            # Score all valid candidates found in text.
+            # Brute force get all ngrams and compare against the candidate list.
             max_len = max_len or len(tokens)
             scores, s_idx, e_idx = [], [], []
             for s, e in tokens.ngrams(n=max_len, as_strings=False):
@@ -268,7 +364,8 @@ class DocReader(object):
                     scores.append(score_s[i][s] * score_e[i][e - 1])
                     s_idx.append(s)
                     e_idx.append(e - 1)
-
+                    
+                # No candidates present
             if len(scores) == 0:
                 pred_s.append([])
                 pred_e.append([])
@@ -284,7 +381,8 @@ class DocReader(object):
                 pred_score.append(scores[idx_sort])
         return pred_s, pred_e, pred_score
 
-   
+       # Saving and loading the model
+
     def save(self, filename):
         if self.parallel:
             network = self.network.module
