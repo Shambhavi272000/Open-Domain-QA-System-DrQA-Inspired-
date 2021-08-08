1b066c54db7c50c0c6e40806a75cdc3ff607df82
Shambhavi <67188688+Shambhavi272000@users.noreply.github.com>
2021-08-08 23:08:29 +0530

    Update interactive.py

--- scripts/reader/interactive.py
+++ scripts/reader/interactive.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-# Copyright 2017-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+
 """A script to run the DrQA reader model interactively."""
 
 import torch
@@ -23,9 +18,7 @@ console.setFormatter(fmt)
 logger.addHandler(console)
 
 
-# ------------------------------------------------------------------------------
 # Commandline arguments & init
-# ------------------------------------------------------------------------------
 
 
 parser = argparse.ArgumentParser()
