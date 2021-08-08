79f2c968ebcd2e182fe6ecc938238a68741ba242
Shambhavi <67188688+Shambhavi272000@users.noreply.github.com>
2021-08-09 01:05:02 +0530

    Update webquestions.py

--- scripts/convert/webquestions.py
+++ scripts/convert/webquestions.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-# Copyright 2017-present, Facebook, Inc.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+
 """A script to convert the default WebQuestions dataset to the format:
 
 '{"question": "q1", "answer": ["a11", ..., "a1i"]}'
@@ -21,11 +16,11 @@ parser.add_argument('input', type=str)
 parser.add_argument('output', type=str)
 args = parser.parse_args()
 
-# Read dataset
+# Reading dataset
 with open(args.input) as f:
     dataset = json.load(f)
 
-# Iterate and write question-answer pairs
+# Iterating and writing question-answer pairs
 with open(args.output, 'w') as f:
     for ex in dataset:
         question = ex['utterance']
